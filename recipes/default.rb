f19d230ff05dc0d42da26976e9982b01b43213c2
Scott Shugh <scott.shugh@hitachivantarafederal.com>
2020-01-09 09:11:39 -0500

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+#file 'C:\\Users\\Administrator\\Documents\\hello.txt' do
+#  content 'greeting=hello chef'
+#end
+
+file '/home/hello.txt' do
+  content 'greeting=hello chef'
+end
